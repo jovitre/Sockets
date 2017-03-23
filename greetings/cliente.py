4f96128b3e9aeeb0a5dc7e1906068bb4eb1b4b83
jovitre <jovitre@gmail.com>
2017-03-22 22:01:54 -0300

    Mudança feita na entrada, agora o nome pode ser dado diretamente como argumento na linha de comando

--- greetings/cliente.py
+++ greetings/cliente.py
@@ -8,7 +8,7 @@ import sys
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = input("Digite seu nome\n")
+MESSAGE = ''.join(sys.argv[1:])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
