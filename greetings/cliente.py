e24afba2fdfb71275fe8dafabb33c5996aa728f5
jovitre <jovitre@gmail.com>
2017-03-25 22:15:56 -0300

    Código alterado para permitir que o cliente entre com nomes compostos e para verificar se o cliente entrou algum nome

--- greetings/cliente.py
+++ greetings/cliente.py
@@ -8,7 +8,14 @@ import sys
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = ''.join(sys.argv[1:])
+MESSAGE = ''
+
+for i in range(1, len(sys.argv)):
+    MESSAGE += sys.argv[i]
+    MESSAGE += ' '
+
+if(len(MESSAGE) < 1):
+    MESSAGE = "você não inseriu o nome na linha de comando"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
--- greetings/servidor.py
+++ greetings/servidor.py
@@ -7,7 +7,7 @@ import socket
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+BUFFER_SIZE = 100  # Normally 1024, but we want fast response
 msg = "Ola, ".encode()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
